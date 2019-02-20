5d476107bd794e9afcff4419875a597f430fc38a
blarsonmoore <blarsonmoore@gmail.com>
2019-02-20 14:52:54 -0700

    netlify test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,45 @@
-import React from "react"
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+const HomePage = () => (
+  <StaticQuery
+    query={graphql`
+      query HomePage {
+        contentfulBlog {
+          title
+
+          content {
+            childMarkdownRemark {
+              html
+            }
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      contentfulBlog: {
+        title,
+
+        content: {
+          childMarkdownRemark: { html }
+        },
+        image: {
+          file: { url }
+        }
+      }
+    }) => (
+      <>
+        <h1>{title}</h1>
+        <img alt="hello" src={url} />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </>
+    )}
+  />
+);
+
+export default HomePage;
